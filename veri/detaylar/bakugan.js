80bfdb4648fc5c290c87d32dd1d2948dae67a399
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2025-11-29 17:59:02 +0300

    Update bakugan.js

--- veri/detaylar/bakugan.js
+++ veri/detaylar/bakugan.js
@@ -1,6 +1,6 @@
 const ANIME_DETAY = {
-  "isim": "Beyblade (1. Sezon)",
-  "ozet": "Takao ve arkadaşlarının efsanevi maceraları başlıyor. Kutsal canavarların gücüyle turnuvanın şampiyonu olabilecekler mi?",
+  "isim": "Bakugan",
+  "ozet": "Dan ve arkadaşlarının efsanevi maceraları başlıyor.",
   "bolumler": [
     {
       "no": 1,
@@ -14,6 +14,21 @@ const ANIME_DETAY = {
     },
     {
       "no": 3,
+      "baslik": "Yeni Bir Arkadaş",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
+    },
+    {
+      "no": 4,
+      "baslik": "Beyblade Savaşçısı Takao",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4976317"
+    },
+    {
+      "no": 5,
+      "baslik": "Ejderin Gücü",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4768205"
+    },
+    {
+      "no": 6,
       "baslik": "Yeni Bir Arkadaş",
       "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
     }
