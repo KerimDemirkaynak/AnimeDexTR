73b4d22521ef7d505e4cbb1303432b37632a69c1
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2025-11-29 20:29:04 +0300

    Update bakugan.js

--- veri/detaylar/bakugan.js
+++ veri/detaylar/bakugan.js
@@ -31,6 +31,66 @@ const ANIME_DETAY = {
       "no": 6,
       "baslik": "Yeni Bir Arkadaş",
       "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
+    },
+    {
+      "no": 7,
+      "baslik": "Beyblade Savaşçısı Takao",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4976317"
+    },
+    {
+      "no": 8,
+      "baslik": "Ejderin Gücü",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4768205"
+    },
+    {
+      "no": 9,
+      "baslik": "Yeni Bir Arkadaş",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
+    },
+    {
+      "no": 10,
+      "baslik": "Beyblade Savaşçısı Takao",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4976317"
+    },
+    {
+      "no": 11,
+      "baslik": "Ejderin Gücü",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4768205"
+    },
+    {
+      "no": 12,
+      "baslik": "Yeni Bir Arkadaş",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
+    },
+    {
+      "no": 13,
+      "baslik": "Beyblade Savaşçısı Takao",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4976317"
+    },
+    {
+      "no": 14,
+      "baslik": "Ejderin Gücü",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4768205"
+    },
+    {
+      "no": 15,
+      "baslik": "Yeni Bir Arkadaş",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
+    },
+    {
+      "no": 16,
+      "baslik": "Beyblade Savaşçısı Takao",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4976317"
+    },
+    {
+      "no": 17,
+      "baslik": "Ejderin Gücü",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4768205"
+    },
+    {
+      "no": 18,
+      "baslik": "Yeni Bir Arkadaş",
+      "link": "https://video.sibnet.ru/shell.php?videoid=4629141"
     }
   ]
 };
