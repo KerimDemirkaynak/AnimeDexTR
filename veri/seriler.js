63b861c67e6178b2852510c9f719e4252ea7a89a
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2025-12-03 21:24:09 +0300

    Update seriler.js

--- veri/seriler.js
+++ veri/seriler.js
@@ -1,14 +1,20 @@
-const TUM_SERILER = [
-  {
-    "id": "beyblade",
-    "isim": "Beyblade",
-    "poster": "https://i.ebayimg.com/images/g/psMAAOSwKFFnq2Ws/s-l400.jpg",
-    "bolumSayisi": "51"
-  },
-  {
-    "id": "bakugan",
-    "isim": "Bakugan",
-    "poster": "https://m.media-amazon.com/images/I/612F8bfniwL._AC_UF894,1000_QL80_.jpg",
-    "bolumSayisi": "276"
-  }
-];
+const TUM_SERILER = [];
+
+for (let i = 0; i < 30; i++) { // 2 seri x 30 = 60
+  TUM_SERILER.push(
+    {
+      "id": `beyblade_${i+1}`,
+      "isim": "Beyblade",
+      "poster": "https://i.ebayimg.com/images/g/psMAAOSwKFFnq2Ws/s-l400.jpg",
+      "bolumSayisi": "51"
+    },
+    {
+      "id": `bakugan_${i+1}`,
+      "isim": "Bakugan",
+      "poster": "https://m.media-amazon.com/images/I/612F8bfniwL._AC_UF894,1000_QL80_.jpg",
+      "bolumSayisi": "276"
+    }
+  );
+}
+
+console.log(TUM_SERILER.length); // 60
